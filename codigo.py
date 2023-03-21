02e9e4d05ef13a605d2f926a165d41a630697422
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-03-21 16:09:49 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -235,25 +235,35 @@ if select_mode == 'Um jogador':
 	lista_times = lista_times.tolist()
 	select_team = st.sidebar.selectbox('Selecione um time', lista_times)
 	
+	#cache para my_df_team
+	@st.cache_data
+	def transform_my_df_team (data, select_team): 
+		
+		# Gerando base my_df apenas com o time selecionado
+		my_df_team = data[data['Time_Jogador'] == select_team]
+		
+		return my_df_team
+	
+	my_df_team = transform_my_df_team (data, select_team)
+	
 	# Adicionando sidebar
-	lista_jogadores = my_df.Nome_Jogador.unique()
+	lista_jogadores = my_df_team.Nome_Jogador.unique()
 	lista_jogadores = lista_jogadores.tolist()
 	select_player = st.sidebar.selectbox('Selecione um jogador', lista_jogadores)
 	st.sidebar.write('Disponível agora video de melhores momentos e estatisticas de cada jogo. Para ver, selecionar *Uma Partida* na primeira caixinha')
 
 	## Cache para my_df
 	@st.cache_data
-	def transform_my_df(data,select_team, select_player):
+	def transform_my_df(my_df_team, select_player):
 	
-		# Gerando base my_df apenas com o time selecionado
-		my_df = data[data['Time_Jogador'] == select_team]
+
 
 		# Gerando my_df apenas para jogador selecionado
-		my_df = my_df[my_df['Nome_Jogador'] == select_player]
+		my_df = my_df_team[my_df_team['Nome_Jogador'] == select_player]
 		my_df.reset_index(inplace = True)
 		return my_df
 	
-	my_df = transform_my_df(data,select_team, select_player)
+	my_df = transform_my_df(my_df_team, select_player)
 
 	# declarando as 4 macro abas do aplicativo
 
