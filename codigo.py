6e76ea65c6257c289006b08b265c24936f55b4cb
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-04-14 15:21:58 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -82,7 +82,7 @@ sheet_id = '1iJMBXj_sbSowhu5sZT3pCLRnSRbydgjbxg-PlEAoZvc'
 sheet_name = 'Acesso'
 url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet='+sheet_name
 data_acesso = pd.read_csv(url)
-df_acesso = pd.DataFrame(data_videos)
+df_acesso = pd.DataFrame(data_acesso)
 
 
 ## Sidebar inicial com seleção do modo do site
@@ -283,7 +283,6 @@ if select_mode == 'Um jogador':
 	
 	# Adicionando lista de partidas ao sidebar
 	df_acesso = df_acesso[df_acesso['Nome_Jogador'] == select_player]
-	df_acesso
 	lista_partidas = df_acesso.Nome_Completo_Partida.unique()
 	lista_partidas = lista_partidas.tolist()
 	select_partida = st.sidebar.selectbox('Selecione uma partida', lista_partidas)
