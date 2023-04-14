7b9f6c4bc29e583e7b4ebe1b13e925d8251dcc09
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-04-14 15:40:52 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -286,7 +286,7 @@ if select_mode == 'Um jogador':
 	lista_partidas = lista_partidas.tolist()
 	lista_partidas_final = ['Todas']
 	for partida in lista_partidas:
-		lista_partidas_final.append(lista_partidas[partida])
+		lista_partidas_final.append(partida)
 	select_partida = st.sidebar.selectbox('Selecione uma partida', lista_partidas_final)
 	
 	st.sidebar.write('Disponível agora video de melhores momentos e estatisticas de cada jogo. Para ver, selecionar *Uma Partida* na primeira caixinha')
